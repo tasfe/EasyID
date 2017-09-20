5ccfc10fd1b8f0b64e72be7f5887f8b752c6f3fc
fup1990 <fup1990@163.com>
2017-09-20 17:28:30 +0800

    去spring

--- easyid-server/src/main/java/com/gome/pop/fup/easyid/server/Server.java
+++ easyid-server/src/main/java/com/gome/pop/fup/easyid/server/Server.java
@@ -17,24 +17,15 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.apache.log4j.Logger;
 import org.apache.zookeeper.KeeperException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * 服务端，接收创建id的请求
  * Created by fupeng-ds on 2017/8/2.
  */
-@Component
-public class Server implements Runnable {
+public class Server {
 
     private static final Logger logger = Logger.getLogger(Server.class);
 
-    private ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-    @Autowired
     private Snowflake snowflake;
 
     private ZkClient zkClient;
@@ -45,7 +36,13 @@ public class Server implements Runnable {
 
     private JedisUtil jedisUtil;
 
+    public Server(String zookeeperAddres, String redisAddress) {
+        this.zookeeperAddres = zookeeperAddres;
+        this.redisAddress = redisAddress;
+    }
+
     public void start() throws Exception {
+        snowflake = new Snowflake();
         jedisUtil = JedisUtil.newInstance(redisAddress);
         String localHost = IpUtil.getLocalHost();
         Cache.set(Constant.LOCALHOST, localHost, -1l);
@@ -53,48 +50,16 @@ public class Server implements Runnable {
         zkClient.register(localHost);
         //查看redis中是否有id,没有则创建
         pushIdsInRedis();
-        //启动服务
-        executorService.submit(this);
         logger.info("EasyID Server started!");
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
                 zkClient.close();
                 jedisUtil.close();
-                executorService.shutdown();
             }
         }));
-    }
-
-    public void run() {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup(8);
-        try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap
-                    .group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-
-                        @Override
-                        protected void initChannel(SocketChannel socketChannel)
-                                throws Exception {
-                            socketChannel.pipeline()
-                                    .addLast(new DecoderHandler(Request.class))
-                                    .addLast(new Handler(jedisUtil, snowflake, zkClient));
-                        }
-                    }).option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true)
-                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
-            ChannelFuture future = bootstrap.bind(IpUtil.getLocalHost(), Constant.EASYID_SERVER_PORT).sync();
-            future.channel().closeFuture().sync();
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error(e.getMessage());
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
+        //启动服务
+        AcceptThread acceptThread = new AcceptThread();
+        acceptThread.start();
     }
 
     private void pushIdsInRedis() throws KeeperException, InterruptedException {
@@ -105,6 +70,42 @@ public class Server implements Runnable {
             long[] ids = snowflake.nextIds((base * 1000) - len.intValue());
             String[] strings = ConversionUtil.longsToStrings(ids);
             jedisUtil.rpush(Constant.REDIS_LIST_NAME, strings);
+        }
+    }
+
+    private class AcceptThread extends Thread {
+
+        @Override
+        public void run() {
+            EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+            EventLoopGroup workerGroup = new NioEventLoopGroup(8);
+            try {
+                ServerBootstrap bootstrap = new ServerBootstrap();
+                bootstrap
+                        .group(bossGroup, workerGroup)
+                        .channel(NioServerSocketChannel.class)
+                        .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                            @Override
+                            protected void initChannel(SocketChannel socketChannel)
+                                    throws Exception {
+                                socketChannel.pipeline()
+                                        .addLast(new DecoderHandler(Request.class))
+                                        .addLast(new Handler(jedisUtil, snowflake, zkClient));
+                            }
+                        }).option(ChannelOption.SO_BACKLOG, 128)
+                        .childOption(ChannelOption.SO_KEEPALIVE, true)
+                        .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                        .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+                ChannelFuture future = bootstrap.bind(IpUtil.getLocalHost(), Constant.EASYID_SERVER_PORT).sync();
+                future.channel().closeFuture().sync();
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.error(e.getMessage());
+            } finally {
+                workerGroup.shutdownGracefully();
+                bossGroup.shutdownGracefully();
+            }
         }
     }
 
