a23d63bce8ebc75f158c1f03f77d67a3f5e8f276
fup1990 <fup1990@163.com>
2017-09-21 09:59:25 +0800

    优化

--- easyid-server/src/main/java/com/gome/pop/fup/easyid/handler/Handler.java
+++ easyid-server/src/main/java/com/gome/pop/fup/easyid/handler/Handler.java
@@ -1,6 +1,7 @@
 package com.gome.pop.fup.easyid.handler;
 
 import com.gome.pop.fup.easyid.model.Request;
+import com.gome.pop.fup.easyid.server.Server;
 import com.gome.pop.fup.easyid.snowflake.Snowflake;
 import com.gome.pop.fup.easyid.util.*;
 import com.gome.pop.fup.easyid.zk.ZkClient;
@@ -8,10 +9,6 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.apache.log4j.Logger;
-import org.apache.zookeeper.KeeperException;
-import redis.clients.jedis.ShardedJedis;
-
-import java.io.IOException;
 
 /**
  * 请求处理handler
@@ -27,28 +24,14 @@ public class Handler extends SimpleChannelInboundHandler<Request> {
 
     private ZkClient zkClient;
 
+    private Server server;
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Request request) throws Exception {
         if (request.getType() == MessageType.REQUEST_TYPE_CREATE) {
             long begin = System.currentTimeMillis();
-            ShardedJedis jedis = jedisUtil.getJedis();
             try {
-                int redis_list_size = zkClient.getRedisListSize();
-                String ip = (String) Cache.get(Constant.LOCALHOST);
-                //zkClient.increase(ip);
-                new Thread(new IncreaseRunnable(zkClient, ip)).start();
-                Long len = jedisUtil.llen(Constant.REDIS_LIST_NAME);
-
-                if (len < (redis_list_size * 300)) {
-                    LOGGER.info("len : " + len);
-                    if (null == len) len = 0l;
-                    //批量生成id
-                    long[] ids = snowflake.nextIds((redis_list_size * 1000) - len.intValue());
-                    String[] strs = ConversionUtil.longsToStrings(ids);
-                    LOGGER.info("ids : " + ids.length);
-                    //将生成的id存入redis队列
-                    jedis.rpush(Constant.REDIS_LIST_NAME, strs);
-                }
+                server.pushIdsInRedis();
                 LOGGER.info("handler run time:" + (System.currentTimeMillis() - begin));
             } finally {
                 jedisUtil.del(Constant.REDIS_SETNX_KEY);
@@ -59,7 +42,8 @@ public class Handler extends SimpleChannelInboundHandler<Request> {
         }
     }
 
-    public Handler(JedisUtil jedisUtil, Snowflake snowflake, ZkClient zkClient) {
+    public Handler(Server server, JedisUtil jedisUtil, Snowflake snowflake, ZkClient zkClient) {
+        this.server = server;
         this.jedisUtil = jedisUtil;
         this.snowflake = snowflake;
         this.zkClient = zkClient;
@@ -87,30 +71,6 @@ public class Handler extends SimpleChannelInboundHandler<Request> {
 
     public void setZkClient(ZkClient zkClient) {
         this.zkClient = zkClient;
-    }
-
-    private class IncreaseRunnable implements Runnable {
-
-        private ZkClient zkClient;
-
-        private String ip;
-
-        public IncreaseRunnable(ZkClient zkClient, String ip) {
-            this.zkClient = zkClient;
-            this.ip = ip;
-        }
-
-        public void run() {
-            try {
-                zkClient.increase(ip);
-            } catch (KeeperException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 }
